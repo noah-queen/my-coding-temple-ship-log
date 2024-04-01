384d2f31a7642bfa395b6148a1942820fbf19d72
noah-queen <noah.queen1098@gmail.com>
2024-04-01 10:26:03 -0400

    updated python file

--- adventure_log.py
+++ adventure_log.py
@@ -1,5 +1,5 @@
 def greet_coding_temple():
-    print ("Ahoy, coding temple navigators! ready for an adventure?") 
+    print ("hello, coding temple navigators! ready for an adventure?") 
            
 if __name__ == "__main__":
     greet_coding_temple()
